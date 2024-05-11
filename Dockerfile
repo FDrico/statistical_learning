4a4f6ae0a341ba59b5a59ad1b6472221a2fcd819
Federico Luna <fede125@gmail.com>
2024-05-11 19:45:58 -0300

    Dockerfile for jupyter notebook and run.sh

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,3 @@ FROM docker.io/jupyter/base-notebook
 
 COPY requirements.txt .
 RUN pip install --user --no-cache-dir -r requirements.txt
-
--- run.sh
+++ run.sh
@@ -2,14 +2,10 @@
 
 while read line; do export $line; done < .env
 
-docker run --rm \
-	-v ./notebooks:/home/jovyan/notebooks -v ./datasets:/home/jovyan/datasets \
+docker run --rm  \
+	-v /home/fede/ISL_python:/home/jovyan/work \
+	-v /home/fede/ISL_python/.jupyter:/home/jovyan/.jupyter \
 	-p 8889:8888 \
-	-e JUPYTER_TOKEN=letmein \
-	-e CHOWN_EXTRA="/home/jovyan/notebooks,/home/jovyan/datasets" \
-    	-e CHOWN_EXTRA_OPTS='-R' \
-	-e GRANT_SUDO=yes \
-	--user root \
 	jupyter \
 	start-notebook.py --NotebookApp.token=${ACCESS_TOKEN}
 
